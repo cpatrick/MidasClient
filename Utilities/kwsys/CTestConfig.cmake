f31bbe39805d10665d185ab6690b98cbae39d0a6
Zach Mullen <zach.mullen@kitware.com>
2010-09-01 14:13:15 -0400

    ENH: Include CTest so that DartConfiguration.tcl is created at config time
    SVN-Revision: 2959

--- Utilities/kwsys/CTestConfig.cmake
+++ Utilities/kwsys/CTestConfig.cmake
@@ -11,7 +11,6 @@
 #=============================================================================
 set (CTEST_PROJECT_NAME "kwsys")
 set (CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
-set (CTEST_DART_SERVER_VERSION "2")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.cdash.org")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=PublicDashboard")
