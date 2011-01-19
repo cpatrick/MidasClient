5b140e7f92112c10f361d934207d3b86275d4c98
Zach Mullen <zach.mullen@kitware.com>
2011-01-19 12:03:29 -0500

    ENH: Improve create profile UI layout
    SVN-Revision: 3372

--- Code/midasUtils.cxx
+++ Code/midasUtils.cxx
@@ -300,18 +300,14 @@ void midasUtils::Tokenize(const std::string& str,
 {
   // Skip delimiters at beginning.
   std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-  // Find first "non-delimiter".
   std::string::size_type pos     = str.find_first_of(delimiters, lastPos);
 
   while (std::string::npos != pos || std::string::npos != lastPos)
     {
     std::string token = str.substr(lastPos, pos - lastPos); 
     if ( trimspaces ) { midasUtils::StringTrim( token ); }
-    // Found a token, add it to the vector.
     tokens.push_back( token );
-    // Skip delimiters.  Note the "not_of"
     lastPos = str.find_first_not_of(delimiters, pos);
-    // Find next "non-delimiter"
     pos = str.find_first_of(delimiters, lastPos);
     }
 }
